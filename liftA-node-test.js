78dfee560c04c92f456a594eab6c25a6e70c8f26
Bill Enright <bill@yisup.com>
2018-05-24 12:48:13 -0700

    latest lifta 15

--- liftA-node-test.js
+++ liftA-node-test.js
@@ -3,7 +3,7 @@ naea = require('./liftA-node');
 
 //const EventEmitter = require('events');
 //const myevents = new EventEmitter();
-const myevents = new (require('events'))();
+const myevents = new(require('events'))();
 
 // a utility function that takes a label and
 // creates an arrow that logs x to the console
@@ -11,7 +11,7 @@ const myevents = new (require('events'))();
 let consoleLogXA = (label) => ((x) => {
   console.log(label, x);
   return x;
-}).liftA();
+}).A;
 
 // this arrow, when run, will wait for the event 'fred' to be fired
 // it will then log the event to the console
@@ -19,10 +19,12 @@ let consoleLogXA = (label) => ((x) => {
 // this is all construction...the arrow is not running yet
 let emitterTest = naea.eventEmitterA(myevents, 'fred')
   .thenA(consoleLogXA('emit fred:'))
-  .firstA();
+  .first;
 
 // in three seconds, emit the fred event
-setTimeout(() => { myevents.emit('fred', 25, 'dog'); }, 3000);
+setTimeout(() => {
+  myevents.emit('fred', 25, 'dog');
+}, 3000);
 
 // run the fred emitter arrow with a pair [1, 'amazing']
 // the arrow only operates on the first of the pair
@@ -31,40 +33,76 @@ let emitterCancel = emitterTest([1, 'amazing'], (x) => {
   console.log('fred event arrow completed x: ', x);
 }, aea.p);
 
-const myevents2 = new (require('events'))();
+const myevents2 = new(require('events'))();
 
 // test emitter with property and value
 let emitterTest2 = naea.eventPropertyEmitterA(myevents2, 'frodo', 'bing', 'bang')
   .thenA(consoleLogXA('emitted frodo'))
-  .firstA();
+  .first;
 
 // the first event will be ignored (we are not looking for 'ding')
 // the second event will be ignored (we are not looking for a value of 'dang')
 // the third event will progress the arrow
-setTimeout(() => { myevents2.emit('frodo', {'ding': 'dang'}); }, 2000);
-setTimeout(() => { myevents2.emit('frodo', {'bing': 'dang'}); }, 2500);
-setTimeout(() => { myevents2.emit('frodo', {'bing': 'bang'}); }, 2900);
+setTimeout(() => {
+  myevents2.emit('frodo', {
+    'ding': 'dang'
+  });
+}, 2000);
+setTimeout(() => {
+  myevents2.emit('frodo', {
+    'bing': 'dang'
+  });
+}, 2500);
+setTimeout(() => {
+  myevents2.emit('frodo', {
+    'bing': 'bang'
+  });
+}, 2900);
 let test2p = aea.P();
 emitterTest2([1, 'real cool'], (x) => {
   console.log('frodo-bing-bang event arrow completed x: ', x);
 }, test2p);
 //setTimeout(() => test2p.cancelAll(), 1500);
 
-const myevents3 = new (require('events'))();
+const myevents3 = new(require('events'))();
 
 // repeat with an emitter - it keeps going...
 let emitterTest3 = naea.eventPropertyEmitterA(myevents3, 'ferris', 'knick', 'knack')
   .thenA(consoleLogXA('ferris'))
-  .firstA()
+  .first
   .thenA(aea.justRepeatA)
-  .repeatA();
+  .repeat;
 
-setTimeout(() => { myevents3.emit('ferris', {'ding': 'dang'}); }, 2000);
-setTimeout(() => { myevents3.emit('ferris', {'knick': 'knack'}); }, 4000);
-setTimeout(() => { myevents3.emit('ferris', {'knick': 'bang'}); }, 5000);
-setTimeout(() => { myevents3.emit('ferris', {'knick': 'knack'}); }, 6000);
-setTimeout(() => { myevents3.emit('ferris', {'knick': 'knack'}); }, 8500);
-setTimeout(() => { myevents3.emit('ferris', {'knick': 'knack'}); }, 9000);
+setTimeout(() => {
+  myevents3.emit('ferris', {
+    'ding': 'dang'
+  });
+}, 2000);
+setTimeout(() => {
+  myevents3.emit('ferris', {
+    'knick': 'knack'
+  });
+}, 4000);
+setTimeout(() => {
+  myevents3.emit('ferris', {
+    'knick': 'bang'
+  });
+}, 5000);
+setTimeout(() => {
+  myevents3.emit('ferris', {
+    'knick': 'knack'
+  });
+}, 6000);
+setTimeout(() => {
+  myevents3.emit('ferris', {
+    'knick': 'knack'
+  });
+}, 8500);
+setTimeout(() => {
+  myevents3.emit('ferris', {
+    'knick': 'knack'
+  });
+}, 9000);
 let test3p = aea.P();
 emitterTest3([1, 'real cool'], () => {}, test3p);
 setTimeout(() => {
@@ -72,20 +110,32 @@ setTimeout(() => {
   test3p.cancelAll();
 }, 8000);
 
-const eventATestEvents = new (require('events'))();
+const eventATestEvents = new(require('events'))();
 let eventATest =
   aea.constA(['fred', eventATestEvents])
   .thenA(naea.eventA)
   .thenA(consoleLogXA('emit fred:'))
   .runA();
-eventATestEvents.emit('dave', { 'one': 1 });
+eventATestEvents.emit('dave', {
+  'one': 1
+});
 eventATestEvents.emit('fred', [3, 4, 5]);
 
 let eventValueATest =
-  aea.constA([{ name: 'dave', property: 'buster', value: 3 }, eventATestEvents])
+  aea.constA([{
+    name: 'dave',
+    property: 'buster',
+    value: 3
+  }, eventATestEvents])
   .thenA(naea.eventValueA)
   .thenA(consoleLogXA('emit fred:'))
   .runA();
-eventATestEvents.emit('dave', { 'buster': 1 });
-eventATestEvents.emit('dave', { 'bluster': 3 });
-eventATestEvents.emit('dave', { 'buster': 3 });
+eventATestEvents.emit('dave', {
+  'buster': 1
+});
+eventATestEvents.emit('dave', {
+  'bluster': 3
+});
+eventATestEvents.emit('dave', {
+  'buster': 3
+});
--- liftA-node.js
+++ liftA-node.js
@@ -25,74 +25,74 @@ SOFTWARE.
 // node.js event emitter arrow
 // one-shot listen for an event
 let eventEmitterA = (emitter, name) => (x, cont, p) => {
-	let cancelId,
-		listener = (e) => {
-			p.advance(cancelId);
-			cont(e, p);
-		};
-	cancelId = p.add(() => emitter.removeListener(name, listener));
-	emitter.once(name, listener);
-	return cancelId;
+  let cancelId,
+    listener = (e) => {
+      p.advance(cancelId);
+      cont(e, p);
+    };
+  cancelId = p.add(() => emitter.removeListener(name, listener));
+  emitter.once(name, listener);
+  return cancelId;
 };
 
 // keep listening until an event that contains property value occurs
 // assumes payload is an object
 let eventPropertyEmitterA = (emitter, name, property, value) => (x, cont, p) => {
-	let cancelId,
-		listener,
-		remove = () => {
-			emitter.removeListener(name, listener);
-		};
-	listener = (e) => {
-		if (e[property] === value) {
-			p.advance(cancelId);
-			remove();
-			cont(e, p);
-		}
-	};
-	cancelId = p.add(remove);
-	emitter.addListener(name, listener);
-	return cancelId;
+  let cancelId,
+    listener,
+    remove = () => {
+      emitter.removeListener(name, listener);
+    };
+  listener = (e) => {
+    if (e[property] === value) {
+      p.advance(cancelId);
+      remove();
+      cont(e, p);
+    }
+  };
+  cancelId = p.add(remove);
+  emitter.addListener(name, listener);
+  return cancelId;
 };
 
 let eventA = (x, cont, p) => {
-	let cancelId,
-		name = x.first(),
-		emitter = x.second(),
-		listener = (e) => {
-			p.advance(cancelId);
-			cont([e, emitter], p);
-		};
-	cancelId = p.add(() => emitter.removeListener(name, listener));
-	emitter.once(name, listener);
-	return cancelId;
+  let cancelId,
+    name = x.first,
+    emitter = x.second,
+    listener = (e) => {
+      p.advance(cancelId);
+      cont([e, emitter], p);
+    };
+  cancelId = p.add(() => emitter.removeListener(name, listener));
+  emitter.once(name, listener);
+  return cancelId;
 };
 
 let eventValueA = (x, cont, p) => {
-	let cancelId,
-		name = x.first().name,
-		property = x.first().property,
-		value = x.first().value,
-		emitter = x.second(),
-		listener,
-		remove = () => {
-			emitter.removeListener(name, listener);
-		};
-	listener = (e) => {
-		if (e[property] === value) {
-			p.advance(cancelId);
-			remove();
-			cont([e, emitter], p);
-		}
-	};
-	cancelId = p.add(remove);
-	emitter.addListener(name, listener);
-	return cancelId;
+  let cancelId,
+    name = x.first.name,
+    property = x.first.property,
+    value = x.first.value,
+    emitter = x.second,
+    listener,
+    remove = () => {
+      emitter.removeListener(name, listener);
+    };
+  listener = (e) => {
+    if (e[property] === value) {
+      p.advance(cancelId);
+      remove();
+      cont([e, emitter], p);
+    }
+  };
+  cancelId = p.add(remove);
+  emitter.addListener(name, listener);
+  return cancelId;
 };
 
 module.exports = {
-	eventA: eventA,
-	eventValueA: eventValueA,
-	eventEmitterA: eventEmitterA,
-	eventPropertyEmitterA: eventPropertyEmitterA
+  eventA: eventA,
+  eventValueA: eventValueA,
+  eventEmitterA: eventEmitterA,
+  eventPropertyEmitterA: eventPropertyEmitterA
 };
